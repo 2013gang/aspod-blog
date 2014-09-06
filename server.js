b1384956ef834d01f84e8aaddfed96e3282726f4
Gang Li <gang.li@turn.com>
2014-09-06 15:00:28 -0700

    remove mongo scripts add gitignore
    Signed-off-by: Gang Li <gang.li@turn.com>

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var app = express(); // create our app w/ express
 var port = process.env.PORT || 5000; // set the port
 
 // configuration ===============================================================
-mongoose.connect(database.url); // connect to mongoDB database on modulus.io
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
